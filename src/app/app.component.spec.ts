f6d7fca413d76a7eecd378a5feebf677645a20a3
Gzyk I.A <ivangzyk@hotmail.com>
2022-05-01 16:14:02 -0300

    soluciona alerta de seletor inesistente

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
@@ -13,6 +14,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent,
       ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA,
+      ]
     }).compileComponents();
   });
 
