c5cc6afef18990291044bfd30cc61bb704c50067
Gzyk I.A <ivangzyk@hotmail.com>
2022-05-01 14:09:04 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'web';
-
-  /**
-   * Retorna `'Bom dia'` se for antes de meio dia.
-   */
-  public geraCumprimento(): string {
-    const dataHoraAtual = new Date();
-    if(dataHoraAtual.getHours() < 12){
-      return 'Bom Dia!'
-    } else {
-      return 'Boa Tarde!';
-    }
-  }
 }
