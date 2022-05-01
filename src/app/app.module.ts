332768792c529ca35af5bf875df403f573d0a419
Gzyk I.A <ivangzyk@hotmail.com>
2022-05-01 15:53:48 -0300

    Formatação com pip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConsultaSaldoComponent } from './components/consulta-saldo/consulta-saldo.component';
+import localPt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
 
+registerLocaleData(localPt, 'pt-BR');
 @NgModule({
   declarations: [
     AppComponent,
+    ConsultaSaldoComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
   ],
   providers: [
+    {
+      provide: LOCALE_ID, useValue: 'pt-BR'
+    },
   ],
   bootstrap: [
     AppComponent,
---
+++ src/app/components/consulta-saldo/consulta-saldo.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from 'src/app/models/cliente';
+
+@Component({
+  selector: 'app-consulta-saldo',
+  templateUrl: './consulta-saldo.component.html',
+  styleUrls: ['./consulta-saldo.component.css']
+})
+export class ConsultaSaldoComponent implements OnInit {
+  public cliente: Cliente = new Cliente(
+    new Date(),
+    'Maria candida',
+    850.00,
+  )
+  constructor(
+
+  ) {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
